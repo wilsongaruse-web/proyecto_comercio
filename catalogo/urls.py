11c4114a89c317ea05b8ac68a6aadf495a2fb6b4
wilsongaruse-web <wilsongaruse@gmail.com>
2026-09-15 09:51:16 -0500

    elaboracion documntacion rest de catalogo y modificaciones panel admin

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
-from rest_framework import routers	
+from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+	
 
 app_name = 'catalogo'
 
@@ -9,4 +11,7 @@ router.register(r'catalogo',views.CatalogoViewSet)
 urlpatterns = [
     path('', views.catalogo, name='catalogo'),
     path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='catalogo:schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='catalogo:schema'), name='redoc'),
 ]
--- comercio/settings.py
+++ comercio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,7 @@ MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
+}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
